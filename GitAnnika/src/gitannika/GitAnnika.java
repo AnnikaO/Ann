02077bf1f1346797d4805e9901ca638087c6e01c
User <User@J0RD132>
2016-11-11 16:02:03 +0100

    first commit

--- GitAnnika/src/gitannika/GitAnnika.java
+++ GitAnnika/src/gitannika/GitAnnika.java
@@ -15,7 +15,7 @@ public class GitAnnika {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello There");
     }
     
 }
